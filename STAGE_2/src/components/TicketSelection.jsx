0afe9d20e497a75e79bb65eb9003ca4f1082c731
Joshuawuro <joshuawuro@gmail.com>
2025-02-14 08:28:35 +0000

    feat:implement usestate to track the errors, selection of tickets and number of tickets

--- STAGE_2/src/components/TicketSelection.jsx
+++ STAGE_2/src/components/TicketSelection.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import EventDetails from "./EventDetails";
 import TicketTypeCard from "./TicketTypeCard";
 import ProgressBar from "./ProgressBar";
 import TicketQuantitySelector from "./TicketQuantitySelector";
 import StepNavigation from "./StepNavigation";
-import { useState } from "react";
 
-const TicketSelection = () => {
+const TicketSelection = ({ onNext, onCancel }) => {
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [quantity, setQuantity] = useState(1);
+  const [error, setError] = useState(false);
+  const [isSelectionComplete, setIsSelectionComplete] = useState(false);
 
   const ticketOptions = [
     { id: 1, type: "Free", price: 0, access: "REGULAR ACCESS", available: 20 },
@@ -15,14 +17,35 @@ const TicketSelection = () => {
     { id: 3, type: "$150", price: 150, access: "VVIP ACCESS", available: 20 },
   ];
 
+  const handleNext = () => {
+    if (!selectedTicket) {
+      setError(true);
+    } else {
+      setError(false);
+      setIsSelectionComplete(true);
+      onNext();
+    }
+  };
+
+  const handleCancel = () => {
+    setSelectedTicket(null);
+    setQuantity(1);
+    setError(false);
+    onCancel();
+  };
+
   return (
-    <div className="bg-lighterblue border-1 border-blue text-white p-6 rounded-2xl max-w-[500px] mx-auto ">
+    <div
+      className={`bg-lighterblue border-1 border-blue text-white p-6 rounded-2xl max-w-[500px] mx-auto ${
+        isSelectionComplete ? "opacity-50 pointer-events-none" : ""
+      }`}
+    >
       <ProgressBar />
 
       <div className="bg-ticketsel p-3 rounded-xl">
         <EventDetails />
 
-        <div className="mt-4 mb-2 border-t-3  border-lightergreen font-roboto">
+        <div className="mt-4 mb-2 border-t-3 border-lightergreen font-roboto">
           <h3 className="mt-4 mb-2 text-sm">Select Ticket Type:</h3>
           <div className="grid md:flex gap-2 p-2 border-2 border-bordergreen rounded-2xl">
             {ticketOptions.map((ticket) => (
@@ -34,12 +57,17 @@ const TicketSelection = () => {
               />
             ))}
           </div>
+          {error && (
+            <p className="text-red-500 text-sm mt-2">
+              Please select a ticket type.
+            </p>
+          )}
         </div>
 
         <h3 className="text-sm mt-4 mb-2">Number of Tickets</h3>
         <TicketQuantitySelector quantity={quantity} onChange={setQuantity} />
 
-        <StepNavigation />
+        <StepNavigation onNext={handleNext} onCancel={handleCancel} />
       </div>
     </div>
   );
