e4b9730f7b3f83a14b5693763a5666e1f6fc11fd
Joshuawuro <joshuawuro@gmail.com>
2025-02-14 08:27:13 +0000

    update the navigation to include the oncancel and noneext functionality

--- STAGE_2/src/components/StepNavigation.jsx
+++ STAGE_2/src/components/StepNavigation.jsx
@@ -1,10 +1,16 @@
-function StepNavigation() {
+function StepNavigation({ onNext, onCancel }) {
   return (
     <div className="grid md:flex justify-between mt-6">
-      <button className="px-4 py-2  rounded-lg text-nextblue md:w-1/2 border border-nextblue mr-1 w-60 mb-1">
+      <button
+        className="px-4 py-2 rounded-lg text-nextblue md:w-1/2 border border-nextblue mr-1 w-60 mb-1"
+        onClick={onCancel}
+      >
         Cancel
       </button>
-      <button className="px-4 py-2 ml-1 bg-nextblue rounded-lg md:w-1/2 mt-1 w-60">
+      <button
+        className="px-4 py-2 ml-1 bg-nextblue rounded-lg md:w-1/2 mt-1 w-60"
+        onClick={onNext}
+      >
         Next
       </button>
     </div>
